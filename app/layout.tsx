abe40e497c2f864d913bebcbea45c5b8917b7fdf
anasalazmeh <anoosalazmeh@gmail.com>
2023-11-25 22:13:24 +0300

    build footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Heading from "./Heading";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Heading/>
         <main>{children}</main>
+        <Footer/>
         </body>
     </html>
   );
