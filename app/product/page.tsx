d38391f976f1d5e7b95464d8af0a3b8b6968c658
anasalazmeh <anoosalazmeh@gmail.com>
2023-12-13 21:01:21 +0300

    fix text in the page product

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -9,7 +9,7 @@ const ProductPage = () => {
   const image=Images
   return (
     <>
-    <p className="flex justify-center mx-2 text-sm items-center my-24 md:text-2xl">ALL PACKAGING BY ADD2ADD FOR ADD2ADD CLIENTS</p>
+    <p className=" mx-2 text-sm my-24 md:text-2xl text-center">ALL PACKAGING BY ADD2ADD FOR ADD2ADD CLIENTS</p>
     <div className="grid grid-cols-1 md:grid-cols-2 mx-8 md:mx-32 my-10 gap-5 justify-center items-center">
       {image.map((item,index)=>(
         <Link href={`/product/${index+1}`} key={index}><CardProduct className="justify-center"  image={item}/></Link> 
